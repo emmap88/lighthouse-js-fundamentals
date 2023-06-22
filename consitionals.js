c954c075fb6481460ec8b799619c934149e83055
EmmaP <emmap8@hotmail.com>
2023-06-21 21:27:11 -0400

    Create consitionals.js
    Add more conditional examples, Emma Perretta.

---
+++ consitionals.js
@@ -0,0 +1,38 @@
+const isCitizen = true;
+const age = 26;
+
+if(isCitizen && age > 18){
+  console.log("You'll are eligible to vote.");
+}
+
+if(isCitizen && age > 18){
+  console.log("You'll are eligible to vote.");
+}
+
+/*const temperature = 12; 
+
+if(temperature < 0)
+{
+  console.log("Make sure you pick out a scarf!");
+} else if(temperature < 15){
+  console.log("Short sleeves won't cut it!");
+}
+else{
+  console.log("Short sleeves are fine.");
+}
+console.log("Now you're ready to go outside!");
+*/
+
+/*
+const raining = true;
+const cold = false; 
+
+if(raining){
+  console.log("Don't forget your umbrella!");
+}
+
+if(cold){
+  console.log("Make sure you pick out a scarf");
+}else{
+console.log("Short sleeves are fine.");
+}*/
