ece1be0e88fd3bf24d7023590623b22420103aa4
EmmaP <emmap8@hotmail.com>
2023-06-26 20:36:40 -0400

    sayHello function practice

---
+++ hello.js
@@ -0,0 +1,19 @@
+// improved code 
+
+const sayHello = function(name){
+
+console.log("Hello, " + name);
+}
+sayHello("Caliban");
+sayHello("Miranda");
+sayHello("Ferdinand");
+
+
+/*  Past code 
+
+const sayHello = function(){
+  console.log("Hello, world");
+}
+sayHello(); 
+
+*/ 
